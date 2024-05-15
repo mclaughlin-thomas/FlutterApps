51e5d12a4544c42b5b9109c462b9b5d5b827f41e
mclaughlin-thomas <90363044+mclaughlin-thomas@users.noreply.github.com>
2024-05-15 15:25:33 -0400

    Grid

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -15,24 +15,15 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false, // remove debug banner
       home: Scaffold(
         backgroundColor: const Color.fromARGB(255, 104, 203, 252),
-        appBar: AppBar(
-          title: Text("My app bar"),
-          backgroundColor: Color.fromARGB(255, 111, 255, 248),
-          elevation: 0,
-          leading: Icon(Icons.menu),
-          actions: [
-            IconButton (
-              onPressed: () {},
-              icon: Icon(Icons.logout),
+        body: GridView.builder(
+            itemCount: 64,
+            gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+            itemBuilder: (context, index) => Container(
+              color: Colors.white,
+              margin: EdgeInsets.all(2),
             ),
-          ],
-        ),
-        body: ListView.builder(
-          itemCount: names.length,
-          itemBuilder: (context, index) => ListTile(
-            title: Text(names[index]),
-          ),
-        ),
+        )
       ), // skelleton widget that holds different parts of app
     );
   }
