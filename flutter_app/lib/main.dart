588e5e14809293aa6843a2e73f4ff3b0eb1456da
mclaughlin-thomas <90363044+mclaughlin-thomas@users.noreply.github.com>
2024-05-15 16:45:50 -0400

    Updated pathes for pages stemming from drawer: home settings

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/pages/first_page.dart';
+import 'package:flutter_app/pages/home_page.dart';
+import 'package:flutter_app/pages/settings_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,6 +18,8 @@ class MyApp extends StatelessWidget {
       home: FirstPage(), // skelleton widget that holds different parts of app
       routes: {
         '/first': (context) => FirstPage(), // route to '/first' page
+        'homepage': (context) => HomePage(), // route to 'homepage' page
+        'settings': (context) => SettingsPage(), // route to 'settings' page
       }
     );
   }
