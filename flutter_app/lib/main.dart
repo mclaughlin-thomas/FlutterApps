8e635dd0c5fca6d023b48a3b14ed1dddb55fff67
mclaughlin-thomas <90363044+mclaughlin-thomas@users.noreply.github.com>
2024-05-15 10:06:28 -0400

    for loop

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
-
-  print(10 % 4); // appears in debug console
 }
 
 //stateless
@@ -17,17 +15,12 @@ class MyApp extends StatelessWidget {
   double height = 5.5;
   bool isMale = false;
 
+
   @override
   Widget build(BuildContext context) {
-    switch(age){
-      case 20:
-        print("Age is 20");
-        break;
-      case 30:
-        print("Age is 30");
-        break;
-      default:
-        print("Age is not 20 or 30");
+
+    for ( int i = 0; i < 10; i++) {
+      print(i);
     }
 
     return const MaterialApp(
