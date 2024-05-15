61cd4060e9048133f22fa5b5ea94aece362641e8
mclaughlin-thomas <90363044+mclaughlin-thomas@users.noreply.github.com>
2024-05-15 10:38:36 -0400

    Removed notes

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -11,33 +11,10 @@ class MyApp extends StatelessWidget {
   MyApp({super.key});
 
 
-  void greet() {
-    print('Hello');
-  }
-
-  void greetPerson(String name){
-    print("Hello " + name);
-  }
   
-  List <int> numbers = [1, 2, 2, 2, 5]; // can have dupes
-  Set <int> setNumbs = {1, 2, 3, 4, 5}; // no dupes
-  //map does key value pairs
-  Map user = {
-    'name': 'John',
-    'age': 30,
-    'isMale': true
-  };
-  void PrintNums() {
-      for (int i = 0; i < setNumbs.length; i++) {
-        print(setNumbs.elementAt(i));
-      }
-    }
 
   @override
   Widget build(BuildContext context) {
-    PrintNums();
-    print(user['name']);
-    print(user['age']);
 
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
