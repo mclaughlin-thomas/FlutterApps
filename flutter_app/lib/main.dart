55846a5d8dc23c3ad82b06d67291664ec00c9b17
mclaughlin-thomas <90363044+mclaughlin-thomas@users.noreply.github.com>
2024-05-15 10:43:57 -0400

    Changing Background color of widget and removing debug banner

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -9,6 +9,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // remove debug banner
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+      ), // skelleton widget that holds different parts of app
+    );
   }
 }
