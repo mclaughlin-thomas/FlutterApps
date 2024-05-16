8d50ef3a4e1562744c940576c53bac5412a4975e
mclaughlin-thomas <90363044+mclaughlin-thomas@users.noreply.github.com>
2024-05-15 20:56:08 -0400

    Completed bottom nav bar | turned it into stateful

--- flutter_app/lib/pages/first_page.dart
+++ flutter_app/lib/pages/first_page.dart
@@ -1,7 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/pages/home_page.dart';
+import 'package:flutter_app/pages/profile_page.dart';
+import 'package:flutter_app/pages/settings_page.dart';
 
-class FirstPage extends StatelessWidget {
-  const FirstPage({super.key});
+
+class FirstPage extends StatefulWidget {
+  FirstPage({super.key});
+
+  @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  //this keeps track of the current page to display
+  int _selectedIndex = 0;
+
+  //updates the navbarbottom
+  void _navigateBottomBar( int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List _pages = [
+    //home page
+    HomePage(),
+    //profile page
+    ProfilePage(),
+    //settings page
+    SettingsPage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +39,11 @@ class FirstPage extends StatelessWidget {
           "First Page"
         ),
       ),
+
+      body: _pages[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _navigateBottomBar,
         items: [ 
           //home
           BottomNavigationBarItem(
