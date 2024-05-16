36e769c76fa0e3ead4b2732f1fddd48ea20ccf1f
mclaughlin-thomas <90363044+mclaughlin-thomas@users.noreply.github.com>
2024-05-15 20:57:54 -0400

    Colored pages

--- flutter_app/lib/pages/profile_page.dart
+++ flutter_app/lib/pages/profile_page.dart
@@ -6,6 +6,7 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.deepOrangeAccent,
       body: Center(
         child: Text( "Profile Page"),
       ),
--- flutter_app/lib/pages/settings_page.dart
+++ flutter_app/lib/pages/settings_page.dart
@@ -6,6 +6,7 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.indigo,
       body: Center(
         child: Text( "Settings Page"),
       ),
