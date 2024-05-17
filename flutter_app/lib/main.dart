975878e6ed4a0f24b1577ed45cf345af777b458d
mclaughlin-thomas <90363044+mclaughlin-thomas@users.noreply.github.com>
2024-05-16 21:37:09 -0400

    Blank app

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/pages/todo_page.dart';
+import 'package:flutter_app/pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ToDoPage(),
+      home: HomePage(),
     );
   }
 }
---
+++ flutter_app/lib/pages/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  String greetingMessage = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
