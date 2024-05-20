ea71564fa66e0c180beaa7fe09a4493e0a1b83bc
mclaughlin-thomas <90363044+mclaughlin-thomas@users.noreply.github.com>
2024-05-20 19:55:50 -0400

    black checkbox

--- flutter_app/lib/util/todo_tile.dart
+++ flutter_app/lib/util/todo_tile.dart
@@ -19,22 +19,26 @@ class ToDoTile extends StatelessWidget {
     
 
     return Padding(
-      padding: const EdgeInsets.all(25.0),
+      padding: const EdgeInsets.only(left: 25.0,right: 25.0, top: 25),
       child: Container(
         padding: const EdgeInsets.all(20.0),
+        decoration: BoxDecoration(
+          color: Colors.yellow,
+          borderRadius: BorderRadius.circular(12),
+        ),
         child: Row(
           children: [
 
             //checkbox
-            Checkbox(value: taskCompleted, onChanged: onChanged),
+            Checkbox(
+              value: taskCompleted,
+              onChanged: onChanged,
+              activeColor: Colors.black,
+              ),
 
             //task name
             Text(taskName),
           ],
-        ),
-        decoration: BoxDecoration(
-          color: Colors.yellow,
-          borderRadius: BorderRadius.circular(12),
         ),
       ),
     );
